7bbba3639e35668ccd62991d335891b378c2a815
felixegdev <felixegdev@gmail.com>
2024-04-02 16:29:23 +0200

    reto 12

--- reto12/index.js
+++ reto12/index.js
@@ -1,38 +1,63 @@
-function decode(message) {
+function checkIsValidCopy(original, copy){
 
-  //we create an array that will store the result as we are creating it.
-  const stack = [];
+  //we first check if the copy is same length than the original
+  //if its not the same legnth its not a valid copy.
+  if(original.length != copy.length) return false;
+  
+  //We create a string chain and store the characters that the copy can degrade to.
+  const deg = '#+:. ';
+  
+  for(let i = 0; i < original.length; i++){
 
-  //we create an array that will be the expected result.
-  let result = '';
+    //We store the characters of the original and the copy into new const
+    const co = original[i];
+    const cc = copy[i];
 
-  //we iterate the message to go char by char.
-  for (const char of message) {
-    //if the char is a '(' that means that all the chars that we have stored in resultwe need to move them into the stack array.
-    if (char === '(') {
-      //we push all we have stored in stack result into stack.
-      stack.push(result);
-      //here we need to restart the result array to start storing the new part of the encrypted message.
-      result = '';
-    } else if (char === ')') {
-      //with the char ')' that means that the encrypted part has ended, and all the chars we have in the 
-      //result array needs to be reversed. 
-      result = stack.pop() + result.split('').reverse().join('');
-    } else {
-      //if we have no parenthesis, we store the chars into the result waiting either way to finish the message, or for the next parenthesis.
-      result += char;
+    //We check if the original has a blank space and the copy doesnt, then is not a valid copy.
+    if(co === ' ' && cc !== ' ')return false;
+
+    /*here we check different posibilities
+    1. if original lower case equals copy lower case, not valid.
+    2. if the original is lower case and copy is upper case, not valid.
+    3. if the original is lower case and copy´s character not a letter and is not in the degradation string, not valid.
+    */
+    if(cc !== ' ' &&
+       co.toLowerCase() === cc.toLowerCase() &&
+       co === co.toLowerCase() &&
+       cc === cc.toUpperCase() &&
+       !deg.includes(cc) &&
+       !/\d/.test(cc) ){
+          return false;
+    }
+    //If original not equal copy in lowecase, and not included in the degradation string, not valid.
+    if(co.toLowerCase() !== cc.toLowerCase() && !deg.includes(cc))return false;
+    
+    //if original degradation is greater than the copy´s degradation, not valid.
+    if(deg.includes(co, cc)){
+      if(deg.indexOf(co) > deg.indexOf(cc)){
+        return false;
+      }
     }
   }
-  //after the loop, we have the whole message decrypted into the array result, that is what we are returning.
-  return result;
+
+  //if everything is valid, then we return true.
+  return true;
 }
 
-// Ejemplos de uso
-const a = decode('hola (odnum)');
-console.log(a); // hola mundo
+checkIsValidCopy(
+  'Santa Claus is coming',
+  'sa#ta Cl#us i+ comin#'
+) // true
+console.log(checkIsValidCopy('Santa Claus is coming','sa#ta Cl#us i+ comin#'));
 
-const b = decode('(olleh) (dlrow)!');
-console.log(b); // hello world!
+checkIsValidCopy(
+  's#nta Cla#s is coming',
+  'p#nt: cla#s #s c+min#'
+) // false (por la p inicial)
+console.log(checkIsValidCopy('s#nta Cla#s is coming','p#nt: cla#s #s c+min#'));
 
-const c = decode('sa(u(cla)atn)s');
-console.log(c); // santaclaus
+checkIsValidCopy('Santa Claus', 's#+:. c:. s') // true
+console.log(checkIsValidCopy('Santa Claus','s#+:. c:. s'));
+
+checkIsValidCopy('Santa Claus', 's#+:.#c:. s') // false (hay un # donde no debería)
+console.log(checkIsValidCopy('Santa Claus', 's#+:.#c:. s'));
