5071e203a4e9571cd4108e6594c4de17816785b9
Felix Eraña Garcia <felixegdev@gmail.com>
2024-04-02 23:39:20 +0200

    READMEs de todos los retos

--- reto5/index.js
+++ reto5/index.js
@@ -1,42 +1,35 @@
-function cyberReindeer(road, time) {
-  const roadArray = road.split('');
-  const result = [road]; // Agregamos el estado inicial de la carretera al resultado
-  let barrierOpened = false;
-  let barrierPosition1 = road.indexOf('|');
-  let barrierPosition2 = road.indexOf('|',barrierPosition1 + 1);
+function cyberReindeer (road, time) {
+  let all = road.split('')
+  const indices = all.map((e, i) => e === '|' ? i : -1).filter(i => i !== -1)
+  const result = []
 
-  for (let i = 1; i <= time; i++) {
-    // Avanzar el trineo una posición a la derecha en cada unidad de tiempo
-    const currentPosition = ((i * time) / road.length);
+  result.push(road)
 
-    // Verificar si la barrera debe abrirse después de cierto tiempo
-    if (i > 5 && !barrierOpened) {
-      roadArray[barrierPosition1] = '*'; // Abrir la barrera
-      roadArray[barrierPosition2] = '*'; // Abrir la barrera
-      barrierOpened = true; // Marcamos que la barrera está abierta
+  for (let i = 0; i < time - 1; i++) {
+    if (i === 4) {
+      all = all.map(b => b === '|' ? '*' : b)
     }
+    const trineoIndex = all.indexOf('S')
 
-    // Actualizar el estado del trineo en la carretera
-    if(barrierOpened){
-      console.log(currentPosition)
-      roadArray[currentPosition] = 'S';
-    }else{
-      console.log(currentPosition,barrierOpened);
-      if(roadArray[currentPosition] === '|'){
-        console.log("barrera");
+    if (all[trineoIndex + 1] !== '|') {
+      if (indices.includes(trineoIndex)) {
+        all[trineoIndex] = '*'
+        all[trineoIndex + 1] = 'S'
+      } else {
+        all[trineoIndex] = '.'
+        all[trineoIndex + 1] = 'S'
       }
     }
-
-    // Agregar el estado actualizado de la carretera al resultado
-    result.push(roadArray.join(''));
-    console.log(roadArray.join(''));
+    result.push(all.join(''))
   }
-
-  return result;
+  return result
 }
 
+const road = 'S..|...|..'
+const time = 10 // unidades de tiempo
+cyberReindeer(road, time)
 // Ejemplo de uso
-const road = 'S..|...|..';
-const time = 10;
+const road1 = 'S..|...|..';
+const time1 = 10;
 const result = cyberReindeer(road, time);
-// console.log(result);
+console.log(result);
